cc1a3944ebaf97941c8b295bc6a564abf65b069e
Pasorra <56963478+Pasorra@users.noreply.github.com>
2023-09-09 02:00:40 +0300

    Symmetric Tree Complete

---
+++ cpp/101. Symmetric Tree/Solution.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    bool isSymmetric(TreeNode *root)
+    {
+        return root && check(root->left, root->right);
+    }
+    bool check(TreeNode *left, TreeNode *right)
+    {
+        if (!left && !right)
+        {
+            return true;
+        }
+        if (!left || !right)
+        {
+            return false;
+        }
+        if (left->val == right->val)
+        {
+            return check(left->left, right->right) && check(left->right, right->left);
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    Solution *s = new Solution();
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(2);
+
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(3);
+    bool res = s->isSymmetric(root);
+    cout << res << endl;
+}
