4e0e7d3efc06eecc6d497db9f0e1d5feee5a11c2
Pasorra <56963478+Pasorra@users.noreply.github.com>
2023-09-09 01:40:32 +0300

    Binary Tree Inorder Traversal Complete

---
+++ cpp/94. Binary Tree Inorder Traversal/Solution.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+        return traverse(root, result);
+    }
+
+    void traverse(TreeNode *root, vector<int> &result)
+    {
+        if (root)
+        {
+            traverse(root->left, result);
+            result.push_back(root->val);
+            traverse(root->right, result);
+        }
+    }
+};
+
+int main()
+{
+    Solution *s = new Solution();
+    TreeNode *root = new TreeNode(1);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+    vector<int> res = s->inorderTraversal(root);
+    for (int val : res)
+    {
+        cout << val << ", ";
+    }
+    cout << endl;
+}
